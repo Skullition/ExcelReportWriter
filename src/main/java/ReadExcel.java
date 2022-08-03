78d6184f2d7c6a31c7c4adfc92ed04bfbd181096
Skullition <78258797+Skullition@users.noreply.github.com>
2022-08-03 17:21:32 +0700

    Add PDFBox dependency and read contents of cell.

--- src/main/java/ReadExcel.java
+++ src/main/java/ReadExcel.java
@@ -1,10 +1,14 @@
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Iterator;
 
 public class ReadExcel {
     public static final Logger LOGGER = LogManager.getLogger();
@@ -13,6 +17,24 @@ public class ReadExcel {
         try {
             XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(args[0]));
             XSSFSheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> iterator = sheet.iterator();
+            while (iterator.hasNext()) {
+                Row row = iterator.next();
+                // Iterate through all column for each row
+                Iterator<Cell> cellIterator = row.cellIterator();
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    switch (cell.getCellType())
+                    {
+                        case NUMERIC:
+                            System.out.println(cell.getNumericCellValue());
+                            break;
+                        case STRING:
+                            System.out.println(cell.getStringCellValue());
+                            break;
+                    }
+                }
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
