0a3446e09fae092fee7610ae45dbf7ba8f94da4f
Skullition <78258797+Skullition@users.noreply.github.com>
2022-08-04 12:52:31 +0700

    Change pdf output dependency to itextpdf and add code to try output.

--- src/main/java/ReadExcel.java
+++ src/main/java/ReadExcel.java
@@ -1,3 +1,9 @@
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfDocument;
+import com.itextpdf.text.pdf.PdfWriter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.poi.ss.usermodel.Cell;
@@ -6,6 +12,7 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
 
@@ -13,6 +20,10 @@ public class ReadExcel {
     public static final Logger LOGGER = LogManager.getLogger();
 
     public static void main(String[] args) {
+        createPdf(args);
+    }
+
+    private static void createPdf(String[] args) {
         try {
             XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(args[0]));
             XSSFSheet sheet = workbook.getSheetAt(0);
@@ -39,13 +50,21 @@ public class ReadExcel {
                         default:
                             throw new IllegalStateException("Unexpected value: " + cell.getCellType());
                     }
-                    if (cellContent.startsWith("Tipe Report")) {
-                        continue;
+
+                    Document document = new Document();
+                    PdfWriter.getInstance(document, new FileOutputStream("TestOutput.pdf"));
+                    document.open();
+                    if (cell.getAddress().getColumn() == 0) {
+                        //
                     }
+
+
                 }
             }
 
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (DocumentException e) {
             e.printStackTrace();
         }
     }
