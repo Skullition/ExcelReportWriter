b4da0cae4f3fe8bc5e37880b255b63d919ced0e3
Skullition <78258797+Skullition@users.noreply.github.com>
2022-08-15 12:52:17 +0700

    Add annotations dependency and convert integer to currency.

--- src/main/java/ReadExcel.java
+++ src/main/java/ReadExcel.java
@@ -8,14 +8,17 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 
 public class ReadExcel {
     public static final Logger LOGGER = LogManager.getLogger();
@@ -25,9 +28,15 @@ public class ReadExcel {
     public static final String GREATNUSA_IMAGE_URL = "https://greatnusa.com/pluginfile.php/1/theme_edumy/headerlogo2/1658542671/Great%20Nusa%20Logo-05_transparen_R.jpg";
     public static final BaseColor DARK_BLUE = new BaseColor(68, 114, 196);
     public static final BaseColor LIGHT_BLUE = new BaseColor(221, 235, 247);
+    public static final NumberFormat IDR_FORMATTER = NumberFormat.getInstance(new Locale("ind"));
 
     public static void main(String[] args) {
         new ReadExcel().createPdf(args);
+    }
+
+    public static String formatStringToIdrCurrency(String money) {
+        String formatted = IDR_FORMATTER.format(Double.valueOf(money));
+        return "Rp. " + formatted;
     }
 
     /**
@@ -35,7 +44,7 @@ public class ReadExcel {
      *
      * @param args the Microsoft Excel document to be read
      */
-    private void createPdf(String[] args) {
+    private void createPdf(String @NotNull [] args) {
         try {
             // Read XSSFWorkbook
             XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(args[0]));
@@ -71,7 +80,7 @@ public class ReadExcel {
         }
     }
 
-    private void CreatePdfBasedOnReportType(List<String> cellValues) throws DocumentException, IOException {
+    private void CreatePdfBasedOnReportType(@NotNull List<String> cellValues) throws DocumentException, IOException {
         String dataType = cellValues.get(0);
         System.out.println(cellValues);
         if (dataType.equalsIgnoreCase("Retail")) {
@@ -87,7 +96,7 @@ public class ReadExcel {
         }
     }
 
-    private void createRetail(List<String> cellValues) throws DocumentException, IOException {
+    private void createRetail(@NotNull List<String> cellValues) throws DocumentException, IOException {
         // Create new Document
         Document document = new Document(PageSize.A4.rotate());
         PdfWriter.getInstance(document, new FileOutputStream(cellValues.get(1) + ".pdf"));
@@ -109,7 +118,7 @@ public class ReadExcel {
         document.close();
     }
 
-    private void addPdfHeader(Document document, String to, String email, String period, String total, boolean isDoubleTable) throws DocumentException, IOException {
+    private void addPdfHeader(@NotNull Document document, String to, String email, String period, String total, boolean isDoubleTable) throws DocumentException, IOException {
         Paragraph headerPhrase = new Paragraph("LAPORAN PENJUALAN KURSUS", new Font(Font.FontFamily.HELVETICA, 16));
         document.add(headerPhrase);
         Image image = Image.getInstance(new URL(GREATNUSA_IMAGE_URL));
@@ -121,12 +130,14 @@ public class ReadExcel {
         document.add(new Paragraph("Kepada  : " + to, font));
         document.add(new Paragraph("Email     : " + email, font));
         document.add(new Paragraph("Periode : " + period, font));
-        Paragraph totalParagraph = new Paragraph("Total     : " + total, font);
+
+        String totalFormatted = formatStringToIdrCurrency(total);
+        Paragraph totalParagraph = new Paragraph("Total     : " + totalFormatted, font);
         totalParagraph.setSpacingAfter(50);
         document.add(totalParagraph);
     }
 
-    private PdfPCell createHeaderCell(String cellContent) {
+    private @NotNull PdfPCell createHeaderCell(String cellContent) {
         Font whiteText = new Font(Font.FontFamily.HELVETICA, 10, 0, BaseColor.WHITE);
         PdfPCell cell = new PdfPCell(new Phrase(cellContent, whiteText));
         cell.setBackgroundColor(DARK_BLUE);
@@ -135,7 +146,7 @@ public class ReadExcel {
         return cell;
     }
 
-    private PdfPCell createBodyCell(String cellContent) {
+    private @NotNull PdfPCell createBodyCell(String cellContent) {
         Font blackText = new Font(Font.FontFamily.HELVETICA, 10);
         PdfPCell cell = new PdfPCell(new Phrase(cellContent, blackText));
         cell.setBackgroundColor(LIGHT_BLUE);
